ef0c613c64f70d5034014f60ccb79e9bd9ca9f72
SaChanPhuoc <phuocscpc06456@fpt.edu.vn>
2024-06-07 09:59:47 +0700

    loi

--- src/main/java/com/vn/controllers/AdminController.java
+++ src/main/java/com/vn/controllers/AdminController.java
@@ -159,6 +159,7 @@ public class AdminController {
 	@ModelAttribute("list_rank")
 	public List<rank> getListRank() {
 		return rankDao.findAll();
+	}
 	
 	@ModelAttribute("list_category")
 	public List<category> getListCategory() {
