8b6023d8d2108f0d90482aff1804aa0c218c7e15
hovu <vunhpc06500@fpt.edu.vn>
2024-06-17 19:10:22 +0700

    chưa xong biến thể

--- src/main/java/com/vn/entity/variant.java
+++ src/main/java/com/vn/entity/variant.java
@@ -1,55 +1,63 @@
-package com.vn.entity;
-
-import java.io.Serializable;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.Data;
-
-@Data
-@Entity
-@Table(name = "VARIANT")
-public class variant implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Integer ID;
+	package com.vn.entity;
 	
-	Integer QUANTITY;
-	Double PRICE;
-
-	@ManyToOne
-	@JoinColumn(name = "ID_PHONE")
-	phone phone;
-
-	@ManyToOne
-	@JoinColumn(name = "ID_COLOR")
-	color color;
-
-	@ManyToOne
-	@JoinColumn(name = "ID_STORAGE")
-	storage storage;
-
-	@ManyToOne
-	@JoinColumn(name = "ID_DISCOUNT_PRODUCT")
-	discount_product discount_product;
-
-	@OneToMany(mappedBy = "variant")
-	@JsonIgnore
-	List<order_item> order_items;
-
-	@OneToMany(mappedBy = "variant")
-	@JsonIgnore
-	List<cart_item> cart_items;
-
-}
+	import java.io.Serializable;
+	import java.util.List;
+	
+	import com.fasterxml.jackson.annotation.JsonIgnore;
+	
+	import jakarta.persistence.CascadeType;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.FetchType;
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.JoinColumn;
+	import jakarta.persistence.ManyToOne;
+	import jakarta.persistence.OneToMany;
+	import jakarta.persistence.Table;
+	import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+	
+	@Data
+	@Entity
+	@Table(name = "VARIANT")
+	public class variant implements Serializable {
+		private static final long serialVersionUID = 1L;
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		Integer ID;
+		
+		@NotNull(message = "Số lượng là bắt buộc")
+		@PositiveOrZero(message = "Số lượng phải lớn hơn 0")
+		Integer QUANTITY;
+		
+		@NotNull(message = "Giá là bắt buộc")
+		@PositiveOrZero(message = "Giá phải lớn hơn 0")
+		Double PRICE;
+			
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "ID_PHONE")
+		phone phone;
+	
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "ID_COLOR")
+		color color;
+	
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "ID_STORAGE")
+		storage storage;
+	
+		@ManyToOne
+		@JoinColumn(name = "ID_DISCOUNT_PRODUCT")
+		discount_product discount_product;
+	
+		@OneToMany(mappedBy = "variant", cascade = CascadeType.ALL)
+		@JsonIgnore
+		List<order_item> order_items;
+	
+		@OneToMany(mappedBy = "variant", cascade = CascadeType.ALL)
+		@JsonIgnore
+		List<cart_item> cart_items;
+	
+	}
