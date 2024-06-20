f655632b1e66d9164518a3f58f91300b32609e39
anh le minh khoi dog <mjkkhoi1@gmail.com>
2024-06-20 17:51:26 +0700

    fix loi order

--- src/main/java/com/vn/DAO/addressDao.java
+++ src/main/java/com/vn/DAO/addressDao.java
@@ -1,6 +1,7 @@
 package com.vn.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vn.entity.address;
 import com.vn.entity.color;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface addressDao extends JpaRepository<address, Integer> {
-
+	@Query("select adr.ADDRESS from address adr where adr.ID = ?1")
+	String findByNameAdr(Integer id);
 }
